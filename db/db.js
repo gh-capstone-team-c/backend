48dd35277a98addc0c97df832a642d4bf83b4ad1
Betsy Groton <grotoned@gmail.com>
2020-10-13 11:23:21 -0400

    fix: debug

--- db/db.js
+++ db/db.js
@@ -1,6 +1,11 @@
 /** @format */
 
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/gobarkbackend');
+const db = new Sequelize(
+	process.env.DATABASE_URL || 'postgres://localhost:5432/gobarkbackend',
+	{
+		logging: false,
+	}
+);
 
 module.exports = db;
