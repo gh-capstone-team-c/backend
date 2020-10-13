752dbab793e6a5083e49e977c7051df6138b8d5d
Betsy Groton <grotoned@gmail.com>
2020-10-13 11:30:32 -0400

    fix: process.env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require('./db/db');
 const User = require("./db/user")
 
