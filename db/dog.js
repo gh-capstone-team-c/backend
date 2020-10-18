a567c7d4ad72a4e281683db996b5e07a29e5c275
Marie Crumpton <marie.k.cr@gmail.com>
2020-10-18 16:17:26 -0400

    chore: added dog color

--- db/dog.js
+++ db/dog.js
@@ -4,26 +4,30 @@ const Sequelize = require('sequelize');
 const db = require('./db');
 
 const Dog = db.define('dog', {
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false,
-		validate: {
-			notEmpty: true,
-		},
-	},
-	happiness: {
-		type: Sequelize.INTEGER,
-		defaultValue: 50,
-		validate: {
-			min: 0,
-			max: 100,
-		},
-	},
-	imageUrl: {
-		type: Sequelize.TEXT,
-		defaultValue:
-			'https://i.pinimg.com/736x/ce/53/c5/ce53c5bcd350ba856e5c53c343376fb2.jpg',
-	},
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  happiness: {
+    type: Sequelize.INTEGER,
+    defaultValue: 50,
+    validate: {
+      min: 0,
+      max: 100,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue:
+      'https://i.pinimg.com/736x/ce/53/c5/ce53c5bcd350ba856e5c53c343376fb2.jpg',
+  },
+  color: {
+    type: Sequelize.ENUM('red', 'cream', 'black-tan'),
+    defaultValue: 'red',
+  },
 });
 
 module.exports = Dog;
--- seed.js
+++ seed.js
@@ -3,116 +3,124 @@
 const { db, User, Dog } = require('./db');
 
 const seed = async () => {
-	try {
-		await db.sync({ force: true });
+  try {
+    await db.sync({ force: true });
 
-		// seed your database here!
+    // seed your database here!
 
-		const admin = await User.create({
-			email: 'admin',
-			password: 'admin',
-			isAdmin: true,
-		});
+    const admin = await User.create({
+      email: 'admin',
+      password: 'admin',
+      isAdmin: true,
+    });
 
-		const betsy = await User.create({
-			email: 'betsy@email.com',
-			password: '123',
-		});
+    const betsy = await User.create({
+      email: 'betsy@email.com',
+      password: '123',
+    });
 
-		const leslie = await User.create({
-			email: 'leslie@email.com',
-			password: '456',
-		});
+    const leslie = await User.create({
+      email: 'leslie@email.com',
+      password: '456',
+    });
 
-		const marie = await User.create({
-			email: 'marie@email.com',
-			password: '789',
-		});
+    const marie = await User.create({
+      email: 'marie@email.com',
+      password: '789',
+    });
 
-		const john = await User.create({
-			email: 'john@email.com',
-			password: '222',
-		});
+    const john = await User.create({
+      email: 'john@email.com',
+      password: '222',
+    });
 
-		const allan = await User.create({
-			email: 'allan@email.com',
-			password: '111',
-		});
+    const allan = await User.create({
+      email: 'allan@email.com',
+      password: '111',
+    });
 
-		const alex = await User.create({
-			email: 'alex@email.com',
-			password: '333',
-		});
+    const alex = await User.create({
+      email: 'alex@email.com',
+      password: '333',
+    });
 
-		const orlando = await User.create({
-			email: 'orlando@email.com',
-			password: '555',
-		});
+    const orlando = await User.create({
+      email: 'orlando@email.com',
+      password: '555',
+    });
 
-		const elle = await User.create({
-			email: 'elle@email.com',
-			password: '222',
-		});
+    const elle = await User.create({
+      email: 'elle@email.com',
+      password: '222',
+    });
 
-		const kody = await Dog.create({
-			name: 'Kody-boo',
-		});
+    const kody = await Dog.create({
+      name: 'Kody-boo',
+      color: 'black-tan',
+    });
 
-		const arya = await Dog.create({
-			name: 'Arya',
-		});
+    const arya = await Dog.create({
+      name: 'Arya',
+      color: 'cream',
+    });
 
-		const hobbes = await Dog.create({
-			name: 'Hobbes',
-		});
+    const hobbes = await Dog.create({
+      name: 'Hobbes',
+      color: 'black-tan',
+    });
 
-		const jodi = await Dog.create({
-			name: 'Jodi',
-		});
+    const jodi = await Dog.create({
+      name: 'Jodi',
+      color: 'black-tan',
+    });
 
-		const sparky = await Dog.create({
-			name: 'Sparky',
-		});
+    const sparky = await Dog.create({
+      name: 'Sparky',
+      color: 'red',
+    });
 
-		const rover = await Dog.create({
-			name: 'Rover',
-		});
+    const rover = await Dog.create({
+      name: 'Rover',
+      color: 'black-tan',
+    });
 
-		const cupcake = await Dog.create({
-			name: 'Cupcake',
-		});
+    const cupcake = await Dog.create({
+      name: 'Cupcake',
+      color: 'cream',
+    });
 
-		const buttercup = await Dog.create({
-			name: 'Buttercup',
-		});
+    const buttercup = await Dog.create({
+      name: 'Buttercup',
+      color: 'cream',
+    });
 
-		//dog associated to user
-		await betsy.setDog(kody);
-		await marie.setDog(hobbes);
-		await leslie.setDog(arya);
-		await john.setDog(jodi);
-		await allan.setDog(sparky);
-		await alex.setDog(rover);
-		await orlando.setDog(cupcake);
-		await elle.setDog(buttercup);
+    //dog associated to user
+    await betsy.setDog(kody);
+    await marie.setDog(hobbes);
+    await leslie.setDog(arya);
+    await john.setDog(jodi);
+    await allan.setDog(sparky);
+    await alex.setDog(rover);
+    await orlando.setDog(cupcake);
+    await elle.setDog(buttercup);
 
-		//added followers
+    //added followers
 
-		await leslie.addFollowing(marie);
-		await marie.addFollowing(leslie);
-		await leslie.addFollowing(alex);
-		await marie.addFollowing(alex);
-		await leslie.addFollowing(orlando);
-		await leslie.addFollowing(betsy);
-		await marie.addFollowing(betsy);
-		await marie.addFollowing(orlando);
-		await betsy.addFollowing(marie);
-		await betsy.addFollowing(leslie);
+    await leslie.addFollowing(marie);
+    await marie.addFollowing(leslie);
+    await leslie.addFollowing(alex);
+    await marie.addFollowing(alex);
+    await leslie.addFollowing(orlando);
+    await leslie.addFollowing(betsy);
+    await marie.addFollowing(betsy);
+    await marie.addFollowing(orlando);
+    await betsy.addFollowing(marie);
+    await betsy.addFollowing(leslie);
 
-		// console.log(Object.keys(betsy.__proto__));
-	} catch (err) {
-		console.log(err);
-	}
+    // console.log(Object.keys(betsy.__proto__));
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = seed;
@@ -120,12 +128,12 @@ module.exports = seed;
 // function, to be used as necessary. But it will run right away if the module
 // is executed directly (e.g. `node seed.js` or `npm run seed`)
 if (require.main === module) {
-	seed()
-		.then(() => {
-			console.log('Seeding success!');
-		})
-		.catch((err) => {
-			console.error('Oh noes! Something went wrong!');
-			console.error(err);
-		});
+  seed()
+    .then(() => {
+      console.log('Seeding success!');
+    })
+    .catch((err) => {
+      console.error('Oh noes! Something went wrong!');
+      console.error(err);
+    });
 }
