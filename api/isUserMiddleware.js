41e43c7bf7655f9942ec06f4fa47c246e4a8aa0b
Betsy Groton <grotoned@gmail.com>
2020-10-19 13:32:24 -0400

    fix: user middleware

--- api/isUserMiddleware.js
+++ api/isUserMiddleware.js
@@ -1,12 +1,14 @@
+/** @format */
+
 //isUser middleware created to protect api routes
 const isUser = (req, res, next) => {
-  if (req.user && req.user.dataValues.id === Number(req.params.userId)) {
-    next()
-  } else {
-    const error = new Error("Sorry, you can't go there!")
-    res.sendStatus(401)
-    next(error)
-  }
-}
+	if (req.user && req.user.dataValues.id === Number(req.params.userId)) {
+		next();
+	} else {
+		const error = new Error("Sorry, you can't go there!");
+		res.sendStatus(401);
+		next(error);
+	}
+};
 
-module.exports = isUser
+module.exports = isUser;
